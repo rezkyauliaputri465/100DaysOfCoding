e13f5c2b88ca99538894d83133d02fc43d3b6cd5
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-02 23:23:03 +0800

    Day001.java

--- Day001.java
+++ Day001.java
@@ -2,7 +2,9 @@ public class days1 {
     public static void main(String[] args) {
         System.out.println("aulia");
         // komentar ini digunakan untuk komentar 1 baris
-        /*komentar ini digunakan untuk lebiih dari 1 baris */
+        /*komentar ini digunakan untuk lebiih
+         dari 1 baris
+        */
         
         
     }
