5649958369acb507f313da2177d2bf5e24ca0332
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-03 18:43:36 +0800

    Day002.java

---
+++ Day002.java
@@ -0,0 +1,18 @@
+public class day002 { // class adalah nama dari class yang sayaa buat
+    public static void main(String[] args) { 
+        /*  psvm adalah tempat untuk 
+        menjalankan classs */
+        
+
+        System.out.printf("Nama : Rezky Aulia \n");
+        System.out.println("Jumlah saudara:  2");
+        System.out.print("Berat Badan : 49.0 kg");
+    
+        
+        // System.out.printf("Berat Badan : 49.0 kg");
+         // println itu untuk menampilkan baris baru 
+         /* print digunakan untuk menampilkan teks 1 baris daan tidak
+          akan membuat baris baru*/
+          // %.1f digunakan untuk angka desimal dengan 1 angka di belakang koma 
+    }
+}
