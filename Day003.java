d9d6985bdb50f2b057fefad8a568ffbdb6906442
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-04 12:56:47 +0800

    Day003.java

---
+++ Day003.java
@@ -0,0 +1,23 @@
+public class day003 {
+    public static void main(String[] args) {
+        System.out.println("Contoh penggunaan Escape Sequence");
+        System.out.println();
+        System.out.println("===== BIODATA AULIA =====");
+        System.out.println("Nama\t\t: Aulia");
+        System.out.println("Nim\t\t: D02225029");
+        System.out.println("Kelas\t\t: C2025");
+
+        System.out.println("\n=====HOBI=====");
+        System.out.println("1.Bermain Volly");
+
+        System.out.println("\"Jangan pernah ragu untuk melangkah!\"");
+        System.out.println("C:\\commit\\isc");
+
+        // \t  digunakan untuk membuat jarak/tab
+        // \n digunakan untuk membuat baris baru
+        // \"  digunakan untuk menampilkan tanda petik
+        // \\ digunakan untuk menampilkan backlash
+    
+
+    }
+}
