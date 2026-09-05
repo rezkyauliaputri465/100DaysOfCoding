bd24f79f31865f09ddfb2dae3b15a5057ba0452f
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-05 15:37:58 +0800

    Day004.java

---
+++ Day004.java
@@ -0,0 +1,17 @@
+public class Day004 {
+    public static void main(String[] args) {
+    byte Umur = 19;
+    short Tahun = 2007;
+    int JumlahKeluarga = 4;
+    long Panjang = 11567; 
+
+    System.out.println("Umur : " + Umur );
+    System.out.println("Tahun : " + Tahun );
+    System.out.println("JumlahKeluarga : " + JumlahKeluarga);
+    System.out.println("Panjang : " + Panjang);
+    }
+   
+    
+    
+
+}
