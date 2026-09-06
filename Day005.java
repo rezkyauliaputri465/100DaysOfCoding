0315878a54fe458457318fe55b8143a703ce58bd
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-06 20:25:30 +0800

    Day005.java

---
+++ Day005.java
@@ -0,0 +1,14 @@
+public class Day005 {
+    public static void main(String[] args) {
+        double Nilai = 97.88d;
+        float Suhu = 20.55f;
+       
+        System.out.println("Nilai  : " + Nilai);
+        System.out.println("Suhu : " + Suhu + "derajat celcius");
+
+        //double digunakan untuk bilangan pecahan yang kapasitas tinggi
+        /*float kebalikan dari daouble,kenapa ditambahkan f karena 
+        nanti akan dibaca floaat jika tidak diberikan*/
+    }
+    
+}
