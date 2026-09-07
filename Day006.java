59427570e19c788e64117e6b03bddbb283949549
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-07 21:34:07 +0800

    Day006.java

---
+++ Day006.java
@@ -0,0 +1,10 @@
+public class Day006 {
+    public static void main(String[] args) {
+        char gender = 'p';
+        boolean rajin = true;
+        System.out.println("gender : " + gender);
+        System.out.println("rajin : " + rajin);
+        // char itu untuk menyimpan satu karakter
+        // boolean itu hanya ada dua kondisi yaitu true dan false
+    }
+}
