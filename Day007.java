b8d873ba9f6b8d25220cd7b8cf7bfacf822126e7
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-08 22:24:35 +0800

    Day007.java

---
+++ Day007.java
@@ -0,0 +1,12 @@
+public class Day007 {
+    public static void main(String[] args) {
+        String perjalanan = "Setiap langkah kecil membawa kita lebih dekat pada impian.";
+        String semangat = "Jangan takut gagal, karena dari kegagalan kita belajar.";
+        String impian = "Terus melangkah, terus berusaha, dan jangan pernah menyerah .";
+
+        System.out.println("Perjalanan : " + perjalanan);
+        System.out.println("Semangat   : " + semangat);
+        System.out.println("Impian     : " + impian);
+        // string digunakan untuk menyimpan teks dan kumpulan kaarakter
+    }
+}
