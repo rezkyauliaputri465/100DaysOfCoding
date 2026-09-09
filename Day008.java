ac498bb4a83954a2da4df7bf2c0d4b9358f69d5b
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-09 19:39:11 +0800

    Day008.java

---
+++ Day008.java
@@ -0,0 +1,16 @@
+public class Day008 {
+    public static void main(String[] args) {
+         // Deklarasi variabel
+        String target;
+
+        // Inisialisasi variabel
+        target = "Menjadi manusia  yang bisa memotivasi ";
+
+        System.out.println("Target Awal : " + target);
+
+        // Update variabel
+        target = "Menjadi orang hebat ";
+
+        System.out.println("Target Baru : " + target);
+    }
+}
