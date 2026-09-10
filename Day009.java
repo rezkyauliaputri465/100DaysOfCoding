8200898c3093a2a0c2a684b1a8018674ac9f7439
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-10 22:51:46 +0800

    Day009.java

---
+++ Day009.java
@@ -0,0 +1,12 @@
+public class Day009 {
+    public static void main(String[] args) {
+        
+        final String SEMANGAT = "Jangan Pernah menyerah hanya karna omongan orang";
+        final String MOTIVASI = "Jangan menyerah sebelum mencoba";
+
+        System.out.println("Semangat : " + SEMANGAT);
+        System.out.println("Motivasi : " + MOTIVASI);
+
+        //final itu variabel yang tidak dapat diubah setelah kita tetapkan
+    }
+}
