23e3374e2f3adaba0c2b833bb05f179ed1a4faf4
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-11 21:11:06 +0800

    day10.java

---
+++ day10.java
@@ -0,0 +1,18 @@
+public class Day10 {
+    public static void main(String[] args) {
+        short batasCommit = 100;
+        long batasPenduduk = 100000;
+
+        System.out.println("Batas Commit : " + batasCommit);
+        System.out.println("Batas Penduduk : " + batasPenduduk);
+
+        System.out.println("Nilai minimal short : " + Short.MIN_VALUE);
+        System.out.println("Nilai maksimal short : " + Short.MAX_VALUE);
+
+        System.out.println("Nilai minimal long : " + Long.MIN_VALUE);
+        System.out.println("Nilai maksimal long : " + Long.MAX_VALUE);
+
+        /*MIN_VALUE digunakan untuk melihat batas nilai minimal, 
+        sedangkan MAX_VALUE digunakan untuk melihat batas nilai maksimal dari suatu tipe data.*/
+    }
+}
