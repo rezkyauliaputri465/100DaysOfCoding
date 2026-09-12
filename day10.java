87001d47682c016fa3b99eee7d2a80cc9aa210ad
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-12 10:00:53 +0800

    day10.java

--- day10.java
+++ day10.java
@@ -1,18 +1,30 @@
-public class Day10 {
+public class day10 {
+    
     public static void main(String[] args) {
-        short batasCommit = 100;
-        long batasPenduduk = 100000;
 
-        System.out.println("Batas Commit : " + batasCommit);
-        System.out.println("Batas Penduduk : " + batasPenduduk);
 
-        System.out.println("Nilai minimal short : " + Short.MIN_VALUE);
-        System.out.println("Nilai maksimal short : " + Short.MAX_VALUE);
-
-        System.out.println("Nilai minimal long : " + Long.MIN_VALUE);
-        System.out.println("Nilai maksimal long : " + Long.MAX_VALUE);
-
-        /*MIN_VALUE digunakan untuk melihat batas nilai minimal, 
-        sedangkan MAX_VALUE digunakan untuk melihat batas nilai maksimal dari suatu tipe data.*/
+        System.out.println("Batas Minimal dan Maksimal Tipe Data");
+        
+        System.out.println("Umur : " + Byte.MIN_VALUE + " sampai " + Byte.MAX_VALUE);
+        System.out.println("Batas Commit : " + Short.MIN_VALUE + " sampai " + Short.MAX_VALUE);
+        System.out.println("Jumlah Siswa : " + Integer.MIN_VALUE + " sampai " + Integer.MAX_VALUE);
+        System.out.println("Jumlah Penduduk : " + Long.MIN_VALUE + " sampai " + Long.MAX_VALUE);
+        System.out.println("Tinggi Badan : " + Float.MIN_VALUE + " sampai " + Float.MAX_VALUE);
+        System.out.println("Harga Barang : " + Double.MIN_VALUE + " sampai " + Double.MAX_VALUE);
+        System.out.println("Huruf : " + (int) Character.MIN_VALUE + " sampai " + (int) Character.MAX_VALUE);
+        System.out.println("Status : " + Boolean.FALSE + " dan " + Boolean.TRUE);
+       
+     // Menampilkan batas minimal dan maksimal tipe data byte untuk umur
+    // Menampilkan batas minimal dan maksimal tipe data short untuk batas commit
+    // Menampilkan batas minimal dan maksimal tipe data int untuk jumlah siswa
+    // Menampilkan batas minimal dan maksimal tipe data long untuk jumlah penduduk
+    // Menampilkan batas minimal dan maksimal tipe data float untuk tinggi badan
+    // Menampilkan batas minimal dan maksimal tipe data double untuk harga barang
+    // Menampilkan batas minimal dan maksimal char dalam bentuk angka
+    // Menampilkan nilai yang dimiliki tipe data boolean, yaitu false dan true
     }
 }
+    
+    
+
+
