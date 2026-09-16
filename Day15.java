8fc8983c39797e662b56e5d655e6a3c7dc8a2d18
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-16 20:36:54 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class day15 {
+    public static void main(String[] args) {
+        
+    
+        int angka1 = 10;
+        int angka2 = 5;
+
+        int hasilKali = angka1 * angka2;
+        int hasilBagi = angka1 / angka2;
+
+        System.out.println(hasilKali);
+        System.out.println(hasilBagi);
+        
+    }
+}
