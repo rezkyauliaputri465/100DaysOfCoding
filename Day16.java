56ebdfc08b41df6fc51fd7850ac7342c7dc39112
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-17 22:40:27 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,9 @@
+public class day16 {
+    public static void main(String[] args) {
+        int a = 40;
+        int b = 4;
+        int hasil = a % b;
+        System.out.println(" Hasil modulus dari " +a+ " % " +b+ " adalah "+ hasil);
+    }
+    
+}
