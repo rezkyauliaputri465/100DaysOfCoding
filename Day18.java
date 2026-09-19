8d918fef8407cca8dc9090575692368817069aea
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-19 19:33:28 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,27 @@
+public class day18 {
+    public static void main(String[] args) {
+        
+        // Tipe data kecil awal (byte)
+        byte angkaKecil = 25;
+
+        // 1. byte ke short
+        short contoh1 = angkaKecil;
+        System.out.println("1. byte ke short  : " + contoh1);
+
+        // 2. byte ke int
+        int contoh2 = angkaKecil;
+        System.out.println("2. byte ke int    : " + contoh2);
+
+        // 3. byte ke long
+        long contoh3 = angkaKecil;
+        System.out.println("3. byte ke long   : " + contoh3);
+
+        // 4. byte ke float
+        float contoh4 = angkaKecil;
+        System.out.println("4. byte ke float  : " + contoh4);
+
+        // 5. byte ke double
+        double contoh5 = angkaKecil;
+        System.out.println("5. byte ke double : " + contoh5);
+    }
+}
