350ac5e2194a9105ff50beb3ed04a4416452f335
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-20 19:42:47 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,15 @@
+public class Day19 {
+    public static void main(String[] args) {
+
+        double angkaDouble = 48.90;
+        int angkaInt = (int) angkaDouble; 
+
+        long angkaLong = 156L;
+        short angkaShort = (short) angkaLong; 
+
+        System.out.println("Nilai awal angka double : " + angkaDouble);
+        System.out.println("Hasil ke int : " + angkaInt);
+        System.out.println("Nilai awal angka long : " + angkaLong);
+        System.out.println("Hasil ke short : " + angkaShort);
+    }
+}
