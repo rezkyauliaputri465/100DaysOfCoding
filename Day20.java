3b7556db32666d1c6d3e67ab4c591089ff5644d6
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-21 21:54:06 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,13 @@
+public class Day20 {
+    public static void main(String[] args) {
+
+        int angka = 80;
+        String hasilInt = String.valueOf(angka);
+
+        double nilai = 44.99;
+        String hasilDouble = String.valueOf(nilai);
+
+        System.out.println("Int menjadi String : " + hasilInt);
+        System.out.println("Double menjadi String : " + hasilDouble);
+    }
+}
