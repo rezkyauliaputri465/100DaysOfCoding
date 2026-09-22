b6b217c45eaf39ae1482865b8fef400b1178ca85
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-22 21:10:23 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,12 @@
+public class Day21 {
+    public static void main(String[] args) {
+
+        String Angka = "80";
+        int angka = Integer.parseInt(Angka);
+        String Nilai = "44.99";
+        double nilai = Double.parseDouble(Nilai);
+
+        System.out.println("String menjadi int : " + angka);
+        System.out.println("String menjadi double : " + nilai);
+    }
+}
