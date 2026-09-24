6aff86e106643b1008f993606deb2ae3e04a3ef9
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-24 12:52:50 +0800

    Day23.java

---
+++ Day23.java
@@ -0,0 +1,9 @@
+public class Day23 {
+    public static void main(String[] args) {
+
+        int sisi = 24;
+        int luas = sisi * sisi;
+        System.out.println("Sisi persegi : " + sisi);
+        System.out.println("Luas persegi : " + luas);
+    }
+}
