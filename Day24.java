ebbbbdf5ab828529fd49ee4ce295e4fbe6cde64f
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-25 17:18:25 +0800

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,13 @@
+public class Day24 {
+    public static void main(String[] args) {
+
+        int panjang = 17;
+        int lebar = 9;
+
+        int luas = panjang * lebar;
+
+        System.out.println("Panjang : " + panjang);
+        System.out.println("Lebar : " + lebar);
+        System.out.println("Luas persegi panjang : " + luas);
+    }
+}
