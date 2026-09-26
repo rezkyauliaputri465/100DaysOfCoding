c7ba4add0ff2598dab981320c33024f8ad2a4d3c
rezkyauliaputri465 <rezkyauliaputri465@gmail.com>
2026-09-26 15:06:46 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,11 @@
+public class Day25 {
+    public static void main(String[] args) {
+
+        double jariJari = 7;
+        double pi = 3.14;
+        double luas = pi * jariJari * jariJari;
+
+        System.out.println("Jari-jari : " + jariJari);
+        System.out.println("Luas lingkaran : " + luas);
+    }
+}
